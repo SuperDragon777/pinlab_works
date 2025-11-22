4a882f244e263362ab78bcfe18a43e359f977e98
SuperDragon777 <Vadim.muromtsev@gmail.com>
2025-11-22 14:31:55 +0300

    I forgot this

--- lcd_display_controller/main.py
+++ lcd_display_controller/main.py
@@ -1,11 +1,19 @@
 from machine import I2C, Pin
 from esp8266_i2c_lcd import I2cLcd
+
 _init()
- 
-DEFAULT_I2C_ADDR = 0x3F # Или 0x27 в зависимости от твоей платы IoT
- 
- 
+
+DEFAULT_I2C_ADDR = 0x3F
+
 i2c = I2C(scl=Pin(5), sda=Pin(4), freq=400000)
 lcd = I2cLcd(i2c, DEFAULT_I2C_ADDR, 2, 16)
 lcd.backlight_on()
-lcd.putstr("Hello, World!\nSecond Line")
+
+print("Enter text for LCD display (Ctrl+C to exit):")
+print("(Use \\n for new line)")
+
+while True:
+    user_text = input("> ")
+    lcd.clear()
+    lcd.putstr(user_text)
+    print("Display output: " + user_text)
